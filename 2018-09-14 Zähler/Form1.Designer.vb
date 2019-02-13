9323380e0f543b5f28e18f42bc0d5741de627eac
Benedikt Michael Schneider <bs762448@fh-muenster.de>
2019-02-13 11:03:41 +0100

    Projektdateien hinzufügen.

---
+++ 2018-09-14 Zähler/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdZähler = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdZähler
+        '
+        Me.cmdZähler.Location = New System.Drawing.Point(276, 109)
+        Me.cmdZähler.Name = "cmdZähler"
+        Me.cmdZähler.Size = New System.Drawing.Size(230, 158)
+        Me.cmdZähler.TabIndex = 0
+        Me.cmdZähler.Text = "Zähler"
+        Me.cmdZähler.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmdZähler)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdZähler As Button
+End Class
---
+++ 2018-09-14 Zähler/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub cmdZähler_Click(sender As Object, e As EventArgs) Handles cmdZähler.Click
+        Dim mA As Integer
+        For mA = 1 To 4 Step 1
+            MsgBox(mA)
+        Next
+        MsgBox("Ende")
+    End Sub
+End Class
